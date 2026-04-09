FIxed chroma average bug; Added and Edited documentation

backend/songs/chord_detector.py
@@ -73,35 +73,50 @@ class ChordDetector:
             return None
         
         chroma = librosa.feature.chroma_cqt(y=segment, sr=sr)
+        avg_chroma = np.mean(chroma, axis=1)
+        
+        chord = self._get_dominant_chord(avg_chroma)
+        quality = self._detect_chord_quality(avg_chroma)
 
-        chord = self._get_dominant_chord(chroma)
-
-        return chord
+        return f'{chord}{quality}'
     
 
-    def _get_dominant_chord(self, chroma):
+    def _get_dominant_chord(self, avg_chroma):
         """
         This method is used to obtain themost dominant chord based on the chroma (frequency representation)
 
         Parameters
         ----------
-        chroma : np.ndarray
-            The chroma features of an audio; the entire spectrum of an audio signal divided into 12 bins
+        avg_chroma : np.ndarray
+            The chrorma features already averaged on axis=1 using np.mean() of an audio (1 dimensional array)
+            The entire spectrum of an audio signal divided into 12 bins (notes)
 
         Returns
         -------
         str
             The note with the most prominent energy/frequency, based on self.NOTES
         """
-
-        avg_chroma = np.mean(chroma, axis=1)
         max_index = np.argmax(avg_chroma)
 
         return self.NOTES[max_index]
     
 
-    def _detect_chord_quality(self, chroma):
-        avg_chroma = np.mean(chroma, axis=1)
+    def _detect_chord_quality(self, avg_chroma):
+        """
+        This method is used to determine whether the chord is a minor or major chord
+        Based on the strength of major 3rd vs minor 3rd
+
+        Parameters
+        ----------
+        avg_chroma : np.ndarray
+            The chrorma features already averaged on axis=1 using np.mean() of an audio (1 dimensional array)
+            The entire spectrum of an audio signal divided into 12 bins (notes)
+        
+        Returns
+        -------
+        str
+            'm' or '' depending on if the chord is a minor or major
+        """
         root_idx = np.argmax(avg_chroma)
 
         major_third_idx = (root_idx + 4) % 12
@@ -110,7 +125,7 @@ class ChordDetector:
         major_strength = avg_chroma[major_third_idx]
         minor_strength = avg_chroma[minor_third_idx]
 
-        if minor_strength > major_strength * 1.2:
+        if minor_strength > (major_strength * 1.2):
             return 'm'
         else:
             return ''