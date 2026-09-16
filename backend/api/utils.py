c2479e1a251e8f10d9b2a8eaa708ed22443fda2e
carlog1566 <cgprojects2508@gmail.com>
2026-09-16 15:02:15 -0700

    Add docstrings to set_auth_cookie helper function

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,6 +1,21 @@
 from decouple import config
 
 def set_auth_cookies(response, access_token, refresh_token):
+    """
+    Helper function to set the user's JWT auth cookies when logging in.
+
+    Takes newly created JWT tokens and assigns them to the user via secure HTTP-only cookies.
+    
+    Parameters
+    ----------
+    access_token : str
+        A newly created JWT authentication token that will be assigned to the user via a secure
+        HTTP-only cookie.
+    refresh_token : str
+        A newly created JWT refresh token that will be assigned to the user via a secure HTTP-only
+        cookie.
+    """
+
     is_production = not config('DEBUG', default=True, cast=bool)
 
     cookie_kwargs = {
